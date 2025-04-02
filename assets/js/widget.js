6f1da44dc2d5647cdf2fdc604ae292d97b049bc1
Axelcxg333 <axelcxg333@gmail.com>
2025-04-01 22:33:06 -0500

    refactor: Cambio en las validaciones
    Se quito una parte de las validaciones para comprobar los datos

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -7,15 +7,17 @@ function updateUserWidget(userData) {
     }
 
     document.querySelectorAll(".widget").forEach(widget => {
-
         widget.querySelector(".user-img").src = userData.picture.data?.url || "default.jpg";
         widget.querySelector(".user-name").innerText = userData.name || "Usuario desconocido";
         widget.querySelector("#profile-link").href = `https://facebook.com/${userData.id || ""}`;
 
         let postsContainer = widget.querySelector("#posts");
-        postsContainer.innerHTML = ""; 
-        if (userData.posts && userData.posts.data && userData.posts.data.length > 0) {
-            let postsHtml = userData.posts.data.map(post => `<p>${post.message || "(Sin texto)"}</p>`).join("<hr>");
+        postsContainer.innerHTML = "";
+        if (userData.posts && userData.posts.data) {
+            let postsHtml = userData.posts.data.map(post => {
+                let postMessage = post.message ? post.message : "Este post no tiene texto";
+                return `<p>${postMessage}</p>`;
+            }).join("<hr>");
             postsContainer.innerHTML = postsHtml;
         } else {
             postsContainer.innerHTML = "<p class='text-center'>No hay publicaciones.</p>";
@@ -23,7 +25,7 @@ function updateUserWidget(userData) {
 
         let imagesContainer = widget.querySelector("#images");
         imagesContainer.innerHTML = "";
-        if (userData.photos && userData.photos.data && userData.photos.data.length > 0) {
+        if (userData.photos && userData.photos.data) {
             let imagesHtml = userData.photos.data.map(photo => `<img src="${photo.images[0]?.source}" class="img-thumbnail m-1" style="width: 100px;">`).join("");
             imagesContainer.innerHTML = imagesHtml;
         } else {
@@ -31,8 +33,8 @@ function updateUserWidget(userData) {
         }
 
         let videosContainer = widget.querySelector("#videos");
-        videosContainer.innerHTML = ""; 
-        if (userData.videos && userData.videos.data && userData.videos.data.length > 0) {
+        videosContainer.innerHTML = "";
+        if (userData.videos && userData.videos.data) {
             let videosHtml = userData.videos.data.map(video => `<video controls src="${video.source}" class="w-100 my-2"></video>`).join("");
             videosContainer.innerHTML = videosHtml;
         } else {
@@ -40,3 +42,5 @@ function updateUserWidget(userData) {
         }
     });
 }
+
+
