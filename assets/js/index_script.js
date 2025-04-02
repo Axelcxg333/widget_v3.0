4c457d03bc74ee99a0ac539869f332cd24af08c9
Axelcxg333 <axelcxg333@gmail.com>
2025-04-01 21:13:56 -0500

    refactor: Variable agregada
    Los datos se pasan a travez de la variable user_datal hacia el widget.js

--- assets/js/index_script.js
+++ assets/js/index_script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     init();
 });
 
+let user_datal = '';
+
 function setupDesignButtons() {
     document.querySelectorAll(".designs button").forEach(button => {
         button.addEventListener("click", function () {
@@ -14,8 +16,7 @@ function setupDesignButtons() {
 
 
 function init() {
-    get_menu('menu_select');
-    get_template('template_01');
+    get_menu('menu_edit');
 }
 
 function get_menu(action) {
@@ -26,7 +27,6 @@ function get_menu(action) {
             action: action
         },
         success: function (response) {
-            console.log("Respuesta del servidor:", response);
             $('#menu').html(response);
             setupDesignButtons(); 
         },
@@ -41,12 +41,12 @@ function get_template(action) {
         url: 'process/control.php',
         method: 'GET',
         data: {
-            action: 'template_0' + action
+            action: 'template_0' + action,
         },
         success: function (response) {
-            console.log("Respuesta del servidor:", response);
             $('#user-info').html(response);
             setupDesignButtons();
+            updateUserWidget(user_datal);
         },
         error: function (xhr, status, error) {
             console.error("Error en la solicitud AJAX:", status, error);
