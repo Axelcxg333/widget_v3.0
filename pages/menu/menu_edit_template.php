3e9504f8b878b7339e8240b6231c1e421cbb9bef
Axelcxg333 <axelcxg333@gmail.com>
2025-04-01 21:16:00 -0500

    perf: Opciones añadidas
    Hemos movido las opciones que se presentaban inicialmente hacia los tabs para mayor comodidad

--- pages/menu/menu_edit_template.php
+++ pages/menu/menu_edit_template.php
@@ -25,6 +25,17 @@
     </div>
     <div class="tab-pane fade" id="layout" role="tabpanel" aria-labelledby="layout-tab">
         <h3>Contenido de Layout</h3>
-        <p>Aquí va el contenido de Layout.</p>
+        <div class="">
+            <label>Selecciona un diseño</label>
+            <div class="designs">
+                <button onclick="get_template(1)">Small Widget</button>
+                <button onclick="get_template(2)">Medium Widget</button>
+                <button onclick="get_template(3)">Facebook Posts</button>
+                <button onclick="get_template(4)">Facebook Videos</button>
+            </div>
+            <button class="btn btn-success" onclick="get_menu('menu_edit')" style="height: fit-content;">
+                Continuar con este diseño
+            </button>
+        </div>
     </div>
 </div>
