0f7cd6b8e327035c4c4d593ac3f4af5fe87483a6
Axelcxg333 <axelcxg333@gmail.com>
2025-04-01 21:15:01 -0500

    perf: Redirecciones mejoradas
    Se cambio la manera de llamar a los datos

--- process/control.php
+++ process/control.php
@@ -2,31 +2,18 @@
 
 $action = $_GET['action'];
 
-switch ($action) {
-    case 'menu_select':
-        include '../pages/menu/menu_select_template.php';
-        exit;
+$templates = [
+    'menu_edit' => '../pages/menu/menu_edit_template.php',
+    'template_01' => '../pages/templates/template_01.php',
+    'template_02' => '../pages/templates/template_02.php',
+    'template_03' => '../pages/templates/template_03.php',
+    'template_04' => '../pages/templates/template_04.php'
+];
 
-    case 'menu_edit':
-        include '../pages/menu/menu_edit_template.php';
-        exit;
-
-    case 'template_01':
-        include '../pages/templates/template_01.php';
-        exit;
-
-    case 'template_02':
-        include '../pages/templates/template_02.php';
-        exit;
-        
-    case 'template_03':
-        include '../pages/templates/template_03.php';
-        exit;
-        
-    case 'template_04':
-        include '../pages/templates/template_04.php';
-        exit;
-
-    default:
-        exit;
+if (array_key_exists($action, $templates)) {
+    include $templates[$action];
+    exit; 
+} else {
+    echo "Acción no válida.";
+    exit;
 }
