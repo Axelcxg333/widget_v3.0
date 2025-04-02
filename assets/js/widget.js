8f5584ad159ec93cdc04dc8d5e695ffaa82110a7
Axelcxg333 <axelcxg333@gmail.com>
2025-04-01 21:12:23 -0500

    feat: Script encargado del procesado de datos
    Este script se encarga de pasar los datos del api de FB hacia los widgets

---
+++ assets/js/widget.js
@@ -0,0 +1,42 @@
+function updateUserWidget(userData) {
+    console.log("Datos recibidos:", userData);
+
+    if (!userData || !userData.picture) {
+        console.error("Los datos del usuario no están completos.");
+        return;
+    }
+
+    document.querySelectorAll(".widget").forEach(widget => {
+
+        widget.querySelector(".user-img").src = userData.picture.data?.url || "default.jpg";
+        widget.querySelector(".user-name").innerText = userData.name || "Usuario desconocido";
+        widget.querySelector("#profile-link").href = `https://facebook.com/${userData.id || ""}`;
+
+        let postsContainer = widget.querySelector("#posts");
+        postsContainer.innerHTML = ""; 
+        if (userData.posts && userData.posts.data && userData.posts.data.length > 0) {
+            let postsHtml = userData.posts.data.map(post => `<p>${post.message || "(Sin texto)"}</p>`).join("<hr>");
+            postsContainer.innerHTML = postsHtml;
+        } else {
+            postsContainer.innerHTML = "<p class='text-center'>No hay publicaciones.</p>";
+        }
+
+        let imagesContainer = widget.querySelector("#images");
+        imagesContainer.innerHTML = "";
+        if (userData.photos && userData.photos.data && userData.photos.data.length > 0) {
+            let imagesHtml = userData.photos.data.map(photo => `<img src="${photo.images[0]?.source}" class="img-thumbnail m-1" style="width: 100px;">`).join("");
+            imagesContainer.innerHTML = imagesHtml;
+        } else {
+            imagesContainer.innerHTML = "<p class='text-center'>No hay imágenes.</p>";
+        }
+
+        let videosContainer = widget.querySelector("#videos");
+        videosContainer.innerHTML = ""; 
+        if (userData.videos && userData.videos.data && userData.videos.data.length > 0) {
+            let videosHtml = userData.videos.data.map(video => `<video controls src="${video.source}" class="w-100 my-2"></video>`).join("");
+            videosContainer.innerHTML = videosHtml;
+        } else {
+            videosContainer.innerHTML = "<p class='text-center'>No hay videos.</p>";
+        }
+    });
+}
